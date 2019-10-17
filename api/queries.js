4f6388844b6c7ab05e499afd7593d75a2963e07b
audeo23 <ludovic.vogel@gmail.com>
2019-10-17 22:15:35 +0200

    API WIP

---
+++ api/queries.js
@@ -0,0 +1,23 @@
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+  user: 'taciiwhwennkcy',
+  host: 'ec2-54-217-206-65.eu-west-1.compute.amazonaws.com',
+  database: 'deb62u7bbkd6qm',
+  password: '8b9ef5e9e4f17f08c675ee7b1e5ceb4534340615a2a828ad726716db7957436e',
+  port: 5432,
+  ssl: true
+})
+
+const getUsers = (request, response) => {
+    pool.query( 'select f.folder_name, f.id as folder_id, p.project_name, p.id as project_id from notes.projects p left join notes.folders f on p.folder_id = f.id', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  module.exports = {
+      getUsers
+  }
--- api/server.js
+++ api/server.js
@@ -1,32 +1,8 @@
 const express = require('express')
 const app = express()
+const db = require('./queries')
 
-const { Client } = require('pg')
-
-const client = new Client({
-  user: 'taciiwhwennkcy',
-  host: 'ec2-54-217-206-65.eu-west-1.compute.amazonaws.com',
-  database: 'deb62u7bbkd6qm',
-  password: '8b9ef5e9e4f17f08c675ee7b1e5ceb4534340615a2a828ad726716db7957436e',
-  port: 5432,
-  host: "localhost",
-  ssl: true
-})
-
-app.get('/', function (req, res) {
-
-  console.log('Requested route /')
-
-  client.query('SELECT NOW() as now', (err, res) => {
-    if (err) {
-      console.log(err.stack)
-    } else {
-      console.log(res.rows[0])
-    }
-  })
-  
-  res.send()
-})
+app.get('/tree', db.getUsers)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
